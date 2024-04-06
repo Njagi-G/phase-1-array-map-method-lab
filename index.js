d5a310228744ef640f85955f83fa28ab9798cd05
Njagi Githaiga <njagigithaiga@gmail.com>
2024-04-06 12:51:51 +0300

    Submitting_array-map_lab

--- index.js
+++ index.js
@@ -11,6 +11,41 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+/*
+//Use
+
+//split() method to split a String object into an array of strings by separating the string into substrings.
+const strOne = 'what does the this keyword mean?'.split(' ');
+console.log(strOne);
+
+//charAt() method to return the specified character from a string.
+const strTwo = 'What is the Constructor OO pattern?'
+console.log(strTwo.charAt(0));
+
+//slice() method to extract a section of a string and returns a new string.
+const strThree = 'implementing Blockchain Web API'
+console.log(strThree.slice(12));
+
+//join() method to join all elements of an array into a string.
+
+*/
+
 const titleCased = () => {
-  return tutorials
-}
+
+  return tutorials.map(title => {
+
+    return title.split(' ').map(word => {
+
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+
+  ).join(' ');
+  },
+  );
+};
+
+titleCased(tutorials);
+
+console.log(titleCased(tutorials));
+
+
